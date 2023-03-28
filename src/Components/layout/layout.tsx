8c630386691abe88a532449d620e1c874f3059bd
Aminn Khatib <khatib.aminn@gmail.com>
2023-03-28 15:38:05 +0200

    fix: layout component for better positioning

--- src/Components/layout/layout.tsx
+++ src/Components/layout/layout.tsx
@@ -1,13 +1,16 @@
 import { Chau_Philomene_One } from "next/font/google";
 import Navigation from "../navigation";
 import Search from "../search";
+import styles from "./layout.module.scss";
 
 function Layout({ children }: { children: JSX.Element }) {
   return (
-    <div>
+    <div className={styles.layout}>
       <Navigation />
-      <Search inputValue={""} />
-      {children}
+      <div className={styles.base}>
+        <Search />
+        {children}
+      </div>
     </div>
   );
 }
