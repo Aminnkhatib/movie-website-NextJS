2a0d3f3524641360308eb153e61fd5db74953d81
Aminn Khatib <khatib.aminn@gmail.com>
2023-03-28 17:23:15 +0200

    feat: add font inter to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout";
 import "@/styles/globals.scss";
+import "@fontsource/inter";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
