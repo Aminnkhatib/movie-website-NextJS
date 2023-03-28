c28ba8bbe89940e207f24f455f15ba155d48ddaa
Aminn Khatib <khatib.aminn@gmail.com>
2023-03-28 17:23:58 +0200

    feat: add nowplaying and toprated cardlists to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.scss";
-import { useEffect, useState } from "react";
+import BigCard from "@/components/cards/bigCard";
+import Title from "@/components/title";
+import SmallCard from "@/components/cards/smallCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ type MovieData = {
   original_title: string;
   original_language: string;
   title: string;
-  backdrop_path: string;
+  backdrop_path: string | null;
   popularity: number;
   vote_count: number;
   video: boolean;
@@ -46,8 +46,8 @@ export const getServerSideProps = async () => {
   return {
     props: {
       trending: response[0].results.slice(0, 2),
-      topRated: response[1].results,
-      nowPlaying: response[2].results,
+      topRated: response[1].results.slice(0, 6),
+      nowPlaying: response[2].results.slice(0, 6),
     },
   };
 };
@@ -63,15 +63,29 @@ export default function Home({ trending, topRated, nowPlaying }: HomeProps) {
 
   return (
     <div>
-      <div className={styles.trending}>
+      <Title titleText="Trending" />
+      <div className={styles.trendingBigCards}>
         {trending.map((data) => (
           <BigCard
             key={data.id}
             // database missing title in some movies
             cardTitle={data.title || data.name}
             cardUnderTitle={data.release_date}
-            src={data.poster_path}
+            src={data.backdrop_path}
           />
+        ))}
+      </div>
+      <Title titleText="Now Playing" />
+      <div className={styles.generalSmallCard}>
+        {nowPlaying.map((data) => (
+          <SmallCard key={data.id} src={data.poster_path} />
+        ))}
+      </div>
+
+      <Title titleText="Top Rated" />
+      <div className={styles.generalSmallCard}>
+        {topRated.map((data) => (
+          <SmallCard key={data.id} src={data.poster_path} />
         ))}
       </div>
     </div>
