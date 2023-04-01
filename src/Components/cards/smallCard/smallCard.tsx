5b814e804f64ace6db6e57218595379dc0967e0c
Aminn Khatib <khatib.aminn@gmail.com>
2023-04-01 19:30:22 +0200

    feat: add styles for fade on smallcard

--- src/Components/cards/smallCard/smallCard.tsx
+++ src/Components/cards/smallCard/smallCard.tsx
@@ -1,6 +1,14 @@
 import styles from "./smallCard.module.scss";
 
-function SmallCard({ src }: { src: string | null }): JSX.Element {
+function SmallCard({
+  src,
+  cardTitle,
+  cardUnderTitle,
+}: {
+  src: string | null;
+  cardTitle: string;
+  cardUnderTitle: string;
+}): JSX.Element {
   return (
     <div className={styles.card}>
       <div
@@ -9,6 +17,10 @@ function SmallCard({ src }: { src: string | null }): JSX.Element {
           backgroundImage: `url(https://image.tmdb.org/t/p/w500${src})`,
         }}
       />
+      <div className={styles.cardTitle}>
+        {cardTitle}
+        <div className={styles.cardUnderTitle}>{cardUnderTitle}</div>
+      </div>
     </div>
   );
 }
