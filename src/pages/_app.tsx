f126f824ca3be1ccfc0221b16eba054e957e18b6
Aminn Khatib <khatib.aminn@gmail.com>
2023-04-16 18:32:11 +0200

    feat: add context to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import Layout from "@/components/layout";
+import { LevelContext } from "@/context/search";
 import "@/styles/globals.scss";
 import "@fontsource/inter";
 import type { AppProps } from "next/app";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [search, setSearch] = useState("");
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <LevelContext.Provider value={{ search, setSearch }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </LevelContext.Provider>
   );
 }
